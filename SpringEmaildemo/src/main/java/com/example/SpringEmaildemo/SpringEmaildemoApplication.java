f5335d47f68135ac774d88e4863abb3a50ca0762
jan-03189 <kaveri.priya@revature.com>
2024-05-28 07:48:23 +0530

    springemail added

---
+++ SpringEmaildemo/src/main/java/com/example/SpringEmaildemo/SpringEmaildemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringEmaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmaildemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmaildemoApplication.class, args);
+	}
+
+}
